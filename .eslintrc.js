d0597bd71ba92b8a93e39ae8b296aec710509c19
Alex <dev.alex.88@gmail.com>
2020-10-16 10:49:19 +0700

    update

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:jest-playwright/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
